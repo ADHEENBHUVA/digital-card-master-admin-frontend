793f00060e3fd9cad86b06829b722ca27bd99707
ADHEEN BHUVA <167649734+ADHEENBHUVA@users.noreply.github.com>
2026-09-18 17:58:40 +0530

    Fix import order syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 import { useEffect } from 'react';
+import useTheme from './hooks/useTheme';
 
 import React, { Suspense, lazy } from 'react';
 
@@ -58,8 +59,6 @@ const AxiosInterceptorProvider = ({ children }) => {
 
     return children;
 };
-
-import useTheme from './hooks/useTheme';
 
 function App() {
     const { theme } = useTheme();
